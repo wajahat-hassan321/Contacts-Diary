5ac9d1527c6ed68a32ce03a70ee3b59a86a306fb
Wajahat Hassan <wajahathassan815@gmail.com>
2024-07-29 18:38:46 +0500

    Update address_keeper.py

--- address_keeper.py
+++ address_keeper.py
@@ -1,5 +1,4 @@
-# Address Book:
-# I Build a simple address book application that allows users to store 
+# I Build a simple contacts book application that allows users to store 
 # contact information (name, phone number, email, etc.)
 # and perform operations like adding new contacts, searching for contacts,
 # and deleting contacts.
